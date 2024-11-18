2af4f4121e0c4a21fc4ade7a3431dc21feb9ceb7
Jabuitron <Jandresbuiitron@gmail.com>
2024-11-18 10:27:18 -0500

    Fix Sale

--- procesosAlmacenados/SP_Sale_Script_Finca.sql
+++ procesosAlmacenados/SP_Sale_Script_Finca.sql
@@ -6,11 +6,35 @@ begin
       insert into tbl_venta(tbl_productos_pro_id, tbl_productos_tbl_proveedor_pro_id, tbl_productos_tbl_categoria_cat_id, tbl_cliente_cli_id, ven_fecha, ven_total) values(vfk_pro_id, tbl_pro_tbl_prov, tbl_pro_tbl_cat, vfk_cliente, v_fecha, v_total);
 end//
 DELIMITER ;
+
+-- Insertar 2 (Simplificado)
+DELIMITER //
+create procedure spInsertSale(IN v_fecha DATE, IN vfk_pro_id INT, IN vfk_cliente int, IN v_total INT)
+begin
+    DECLARE prov INT;
+    DECLARE cat INT;
+
+    SELECT tbl_proveedor_pro_id, tbl_categoria_cat_id
+    INTO prov, cat
+    from tbl_productos where tbl_productos.pro_id = vfk_pro_id;    
+    insert into tbl_venta(ven_fecha, tbl_productos_pro_id, tbl_cliente_cli_id, ven_total, tbl_productos_tbl_proveedor_pro_id, tbl_productos_tbl_categoria_cat_id) values(v_fecha, vfk_pro_id,  vfk_cliente, v_total, prov, cat);
+end//
+DELIMITER ;
 -- Mostrar
 DELIMITER //
 create procedure procSelectSale()
 begin
-     select tbl_productos_pro_id, tbl_productos_tbl_proveedor_pro_id, tbl_productos_tbl_categoria_cat_id, tbl_cliente_cli_id, ven_fecha, ven_total from tbl_venta;
+    select
+     ven_id as 'Venta ID',
+     ven_fecha as 'Fecha',
+     concat(tbl_productos.pro_id, ' ', tbl_productos.pro_nombre) as 'ID / Nombre del Producto',
+	 concat(tbl_cliente.cli_id, ' ', tbl_cliente.cli_nombre) as 'ID / Nombre del Cliente',
+     ven_total as 'Total'
+     FROM tbl_cliente
+	 JOIN tbl_venta
+        ON tbl_cliente.cli_id = tbl_venta.tbl_cliente_cli_id
+	 JOIN tbl_productos
+        ON tbl_productos.pro_id = tbl_venta.tbl_productos_pro_id;  
 end//
 DELIMITER ;
 -- Actualizar
